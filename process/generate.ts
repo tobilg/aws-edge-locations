0a70964b66c013370de9a4fc442c153b7c2f7104
tobilg <tobilg@gmail.com>
2025-01-05 13:30:06 +0100

    Refactoring

--- process/generate.ts
+++ process/generate.ts
@@ -61,34 +61,33 @@ const parquetSchema = new parquet.ParquetSchema({
   pricingRegion: { type: 'UTF8', optional: true },
 });
 
-const writeParquet = async locations => {
-  const parquetPath = join(__dirname, '../', 'data', 'aws-edge-locations.parquet');
+const writeParquet = async (locations: EdgeLocation[], filename: string = 'aws-edge-locations') => {
+  const parquetPath = join(__dirname, '../', 'data', `${filename}.parquet`);
   const writer = await parquet.ParquetWriter.openFile(parquetSchema, parquetPath);
 
   try {
+    // Filter out locations without codes and write valid rows
+    const validLocations = locations.filter(location => location.code !== undefined);
+    for (const location of validLocations) {
+      await writer.appendRow({
+        code: location.code || '',  // Provide empty string as fallback
+        city: location.city || '',
+        country: location.country || '',
+        countryCode: location.countryCode || '',
+        latitude: location.latitude || 0,
+        longitude: location.longitude || 0,
+        pricingRegion: location.pricingRegion || '',
+      });
+    }
+  } catch (error) {
+    console.error('Error writing parquet:', error);
+  } finally {
+    await writer.close();
+  }
+};
 
-  
-
-  locations.forEach(async location => {
-    await writer.appendRow({
-      code: location.code,
-      city: location.city,
-      country: location.country,
-      countryCode: location.countryCode,
-      latitude: location.latitude,
-      longitude: location.longitude,
-      pricingRegion: location.pricingRegion,
-    })
-  });
-
-} catch (error) {
-  console.log(error)
-}
-  await writer.close();
-}
-
-const writeCSV = locations => {
-  const csvPath = join(__dirname, '../', 'data', 'aws-edge-locations.csv');
+const writeCSV = (locations: EdgeLocation[], filename: string = 'aws-edge-locations') => {
+  const csvPath = join(__dirname, '../', 'data', `${filename}.csv`);
   const data = locations.map(e => {
     return `${e.code},${e.city},${e.country},${e.countryCode},${e.latitude},${e.longitude},"${e.pricingRegion}"`
   });
@@ -97,8 +96,8 @@ const writeCSV = locations => {
   writeFileSync(csvPath, data.join(EOL), 'utf8');
 }
 
-const writeJSON = (locations: EdgeLocation[], directory: string = 'data') => {
-  const jsonPath = join(__dirname, '../', directory, 'aws-edge-locations.json');
+const writeJSON = (locations: EdgeLocation[], filename: string = 'aws-edge-locations') => {
+  const jsonPath = join(__dirname, '../', 'data', `${filename}.json`);
   const data: EdgeLocations = {};
   locations.forEach(location => {
     data[location.code!] = {
@@ -157,54 +156,65 @@ const lookupPricingRegion = (location) => {
   return null;
 }
 
+const enrichLocation = (location: { city: string | null | undefined; country: string | null | undefined; }): EdgeLocation => {
+  const mappedCity = location.city && nameMapping.hasOwnProperty(location.city) ? nameMapping[location.city] : location.city;
+
+  // Initialize edge location
+  const enrichedEdgeLocation: EdgeLocation = {
+    code: undefined,
+    city: mappedCity,
+    country: location.country || '',
+    countryCode: '',
+    latitude: 0,
+    longitude: 0,
+    pricingRegion: ''
+  };
+
+  // Get location from airport data
+  const airport = lookupAirport(utf8.encode(mappedCity));
+
+  // Apply location data from overrides or airport database
+  if (airportOverrides.hasOwnProperty(mappedCity.toLowerCase() || '')) {
+    const overrideData = airportOverrides[mappedCity.toLowerCase() || ''];
+    Object.assign(enrichedEdgeLocation, {
+      code: overrideData.code,
+      countryCode: overrideData.countryCode,
+      latitude: overrideData.latitude,
+      longitude: overrideData.longitude
+    });
+  } else if (airport) {
+    const [latitude, longitude] = airport.coordinates.split(', ').map(parseFloat);
+    Object.assign(enrichedEdgeLocation, {
+      code: airport.iata_code,
+      countryCode: airport.iso_country,
+      latitude,
+      longitude
+    });
+  }
+
+  // Add pricing region
+  enrichedEdgeLocation.pricingRegion = lookupPricingRegion(location) || '';
+  
+  return enrichedEdgeLocation;
+};
+
 const run = async () => {
   const locationsData = await downloadAsHTMLWithPuppeteer();
 
-  const edgeLocations = locationsData.filter(e => e.type === 'Edge Locations').map(e => e.locations).flat();
+  const rawEdgeLocations = locationsData.filter(e => e.type === 'Edge Locations').map(e => e.locations).flat();
 
-  const fullEdgeLocations: EdgeLocation[] = [];
+  //const rawPopLocations = locationsData.filter(e => e.type === 'Embedded POPs').map(e => e.locations).flat();
 
-  edgeLocations.forEach((location)  => {
-    const mappedCity = location.city && nameMapping.hasOwnProperty(location.city) ? nameMapping[location.city] : location.city;
-
-    // Instatiate
-    let fullEdgeLocation: EdgeLocation = {
-      code: undefined,
-      city: mappedCity,
-      country: location.country || '',
-      countryCode: '',
-      latitude: 0,
-      longitude: 0,
-      pricingRegion: ''
-    };
-
-    // Get location
-    const airport = lookupAirport(utf8.encode(mappedCity));
-
-    // Check for overrides
-    if (airportOverrides.hasOwnProperty(mappedCity.toLowerCase() || '')) {
-      const overrideData = airportOverrides[mappedCity.toLowerCase() || ''];
-      fullEdgeLocation.code = overrideData.code;
-      fullEdgeLocation.countryCode = overrideData.countryCode;
-      fullEdgeLocation.latitude = overrideData.latitude;
-      fullEdgeLocation.longitude = overrideData.longitude;
-    } else if (airport) {
-      fullEdgeLocation.code = airport.iata_code;
-      fullEdgeLocation.countryCode = airport.iso_country;
-      const coordinate = airport.coordinates.split(', ');
-      fullEdgeLocation.latitude = parseFloat(coordinate[0]);
-      fullEdgeLocation.longitude = parseFloat(coordinate[1]);
-    }
-    // Get pricing region
-    fullEdgeLocation.pricingRegion = lookupPricingRegion(location) || '';
-    // Push to array
-    fullEdgeLocations.push(fullEdgeLocation);
-  });
+  const edgeLocations: EdgeLocation[] = rawEdgeLocations.map(enrichLocation);
+  //const popLocations: EdgeLocation[] = rawPopLocations.map(enrichLocation);
 
   // Write to data
-  writeJSON(fullEdgeLocations);
-  writeCSV(fullEdgeLocations);
-  writeParquet(fullEdgeLocations);
+  writeJSON(edgeLocations);
+  //writeJSON(popLocations, 'aws-pop-locations');
+  writeCSV(edgeLocations);
+  //writeCSV(popLocations, 'aws-pop-locations');
+  writeParquet(edgeLocations);
+  //writeParquet(popLocations, 'aws-pop-locations');
 };
 
 run();
--- scripts/load-airports.sql
+++ scripts/load-airports.sql
@@ -3,3 +3,4 @@ LOAD json;
 
 COPY (select * from 'https://r2.datahub.io/clt98lrmc000fl708ilem2s44/master/raw/data/airport-codes.csv') TO 'temp/airport-codes.json' (ARRAY true);
 COPY (select * from 'https://r2.datahub.io/clt98lrmc000fl708ilem2s44/master/raw/data/airport-codes.csv' where type='large_airport') TO 'temp/large-airports.json' (ARRAY true);
+COPY (select * from 'https://r2.datahub.io/clt98lrmc000fl708ilem2s44/master/raw/data/airport-codes.csv' where iata_code is not null and type like '%_airport') TO 'temp/iata-airports.json' (ARRAY true);
